df5df2be35ba1443153dc70652a17cca3cf79833
laasyapriya2006 <laasyal2006@gmail.com>
2020-09-02 14:54:17 +0530

    added constraints

---
+++ chain.js
@@ -0,0 +1,17 @@
+class Chain{
+    constructor(bodyAobj,bodyBobj){
+        var options = {
+            bodyA: bodyAobj,
+            bodyB: bodyBobj,
+            stiffness: 0.04,
+            length: 10
+        };
+        this.chain = Matter.Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    display(){
+        strokeWeight(4);
+        line(this.chain.bodyA.position.x , this.chain.bodyA.position.y,
+            this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+// namespace matter objects with constant
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -5,6 +6,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -12,29 +14,35 @@ function preload() {
 
 function setup(){
     var canvas = createCanvas(1200,400);
+    //create an engine and a world
     engine = Engine.create();
     world = engine.world;
 
-
+    //created ground and platform
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
+    //created the first level of objects
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
+    //level 2
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
-
     log3 =  new Log(810,180,300, PI/2);
 
+    //level 3
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    log6 = new Log(200,200,150,PI/2);
+    
+    //created bird and chain
     bird = new Bird(100,100);
+    chain1 = new Chain(bird.body,log6.body);
 
 }
 
@@ -58,7 +66,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
+    chain1.display();
 }
